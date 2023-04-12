8b28480100d385d3f4ceb4baa157a33d8e79e8af
rooneyeno <swift2passion@gmail.com>
2023-04-12 10:53:12 +0000

    shapes exercises

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,17 +1,17 @@
 #!/usr/bin/node
 const dict = require('./101-data').dict;
-
-const totalist = Object.entries(dict);
-const vals = Object.values(dict);
-const valsUniq = [...new Set(vals)];
-const newDict = {};
-for (const j in valsUniq) {
-  const list = [];
-  for (const k in totalist) {
-    if (totalist[k][1] === valsUniq[j]) {
-      list.unshift(totalist[k][0]);
-    }
-  }
-  newDict[valsUniq[j]] = list;
+let newDict = {};
+let k;
+for (k in dict) {
+  newDict[dict[k]] = [];
+}
+for (k in dict) {
+  newDict[dict[k]].push(k);
+}
+function cmp (a, b) {
+  return a - b;
+}
+for (k in newDict) {
+  newDict[k].sort(cmp);
 }
 console.log(newDict);
